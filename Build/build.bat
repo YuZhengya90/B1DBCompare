16d5bd1ac97a00828fe44bb00138d468d519e8de
YuZhengya90 <YuZhengya90@users.noreply.github.com>
2019-08-08 17:28:13 +0800

    auto build.bat

--- Build/build.bat
+++ Build/build.bat
@@ -1,21 +1,25 @@
-set /p version=please set versionNum:
+set /p version=please set versionNum (E.G majorVersion.minorVersion.FixVersion):
 echo %version% > version.txt
 
+set workingDir=%cd%
+
+for /f "delims=[" %%i in (vspath.txt) do set vsPath=%%i
+
 C:
-cd C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE
-devenv D:\B1DBCompare\B1DBCompare.sln /Build "Release|X64"
-devenv D:\B1DBCompare\B1DBCompare.sln /Build "Release|X86"
+cd %vsPath%
+devenv %workingDir%\..\B1DBCompare.sln /ReBuild "Release|X64"
+devenv %workingDir%\..\B1DBCompare.sln /ReBuild "Release|X86"
 D:
 cd ..\Output.64
 
-rar a -r B1_DB_Compare_V%version%_X64.zip Release
+..\Tools\rar.exe a -r B1_DB_Compare_V%version%_X64.zip Release
 cd ..\Output.86
-rar a -r B1_DB_Compare_V%version%_X86.zip Release
+..\Tools\rar.exe a -r B1_DB_Compare_V%version%_X86.zip Release
 
 cd ..\B1DBCompare Versions
 mkdir V%version%
 copy ..\Output.64\B1_DB_Compare_V%version%_X64.zip V%version%
 copy ..\Output.86\B1_DB_Compare_V%version%_X86.zip V%version%
 cd V%version%
-echo nul > "whats new.txt"
+echo README (VERSION_%version%) > "whats new.txt"
 pause
